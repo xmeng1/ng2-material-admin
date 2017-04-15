6e568b6a064cf61ff6b32488717c12be6c7a602c
Xin Meng <x.meng@outlook.com>
2017-04-15 22:47:35 +0100

    update the package
    1. the tslint still use the 4.5.X, because the 5.X has issue.
    2. add debug the angular version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, VERSION} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'app works!';
+  constructor() {
+    console.log(VERSION.full);
+  }
+
 }
