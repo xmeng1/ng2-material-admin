e9e6fc1a291261d2934e367011b22cfa1daccb6e
Xin Meng <x.meng@outlook.com>
2017-04-15 22:55:00 +0100

    import the angular material 2 component support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MdAutocompleteModule, MdButtonModule, MdButtonToggleModule, MdCardModule, MdCheckboxModule, MdChipsModule,
+  MdCoreModule,
+  MdDialogModule, MdGridListModule, MdIconModule,
+  MdInputModule,
+  MdLineModule, MdListModule, MdMenuModule, MdOptionModule, MdProgressBarModule, MdProgressSpinnerModule, MdRadioModule,
+  MdRippleModule,
+  MdSelectionModule,
+  MdSelectModule, MdSidenavModule, MdSliderModule, MdSlideToggleModule, MdSnackBarModule, MdTabsModule, MdToolbarModule,
+  MdTooltipModule,
+  OverlayContainer
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -12,9 +25,43 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    BrowserAnimationsModule,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdInputModule,
+    MdSelectModule,
+    MdDialogModule,
+    MdAutocompleteModule,
+    MdCardModule,
+    MdChipsModule,
+    MdLineModule,
+    MdMenuModule,
+    MdIconModule,
+    MdListModule,
+    MdOptionModule,
+    ReactiveFormsModule,
+    MdButtonToggleModule,
+    MdGridListModule,
+    MdProgressSpinnerModule,
+    MdSlideToggleModule,
+    MdSidenavModule,
+    MdToolbarModule,
+    MdSnackBarModule,
+    MdProgressBarModule,
+    MdTooltipModule,
+    MdTabsModule,
+    MdSliderModule,
+    MdSelectionModule,
+    MdRippleModule,
+    MdRadioModule,
+    MdCoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public overlayContainer: OverlayContainer) {
+    overlayContainer.themeClass = 'red-indigo';
+  }
+}
